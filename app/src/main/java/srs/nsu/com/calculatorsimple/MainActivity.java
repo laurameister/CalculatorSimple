e5e940e05943d529b47bf1d363dc62b8776778c6
pwntcp <rafi8800@gmail.com>
2015-11-09 21:50:04 +0600

    (9/11/15)- First commit

--- app/src/main/java/srs/nsu/com/calculatorsimple/MainActivity.java
+++ app/src/main/java/srs/nsu/com/calculatorsimple/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    EditText e = (EditText) findViewById(R.id.text_edit);
+
+    public void onClickOne(View view){
+
     }
 
     @Override
