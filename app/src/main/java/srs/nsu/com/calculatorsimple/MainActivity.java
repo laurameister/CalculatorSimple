4cf2aca0e4c0ca7a69c982117c85c5d655272d61
pwntcp <rafi8800@gmail.com>
2015-11-10 14:25:09 +0600

    Backspace function need work

--- app/src/main/java/srs/nsu/com/calculatorsimple/MainActivity.java
+++ app/src/main/java/srs/nsu/com/calculatorsimple/MainActivity.java
@@ -6,9 +6,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+ //   TextView e = (TextView) findViewById(R.id.text_edit);
+    //TextView t = (TextView) findViewById(R.id.show_prev);
+    //TextView u = (TextView) findViewById(R.id.show_operator);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +21,175 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    EditText e = (EditText) findViewById(R.id.text_edit);
 
-    public void onClickOne(View view){
+   public void onClickOne(View view){
+       TextView e = (TextView) findViewById(R.id.text_edit);
+       String x = (String) e.getText();
+       e.setText(x + "1");
+   }
+    public void onClickTwo(View view){
+    TextView e = (TextView) findViewById(R.id.text_edit);
+        String x = (String) e.getText();
+        e.setText(x+"2");
+    }
+    public void onClickThree(View view){
+    TextView e = (TextView) findViewById(R.id.text_edit);
+        String x = (String) e.getText();
+        e.setText(x+"3");
+    }
+    public void onClickFour(View view){
+    TextView e = (TextView) findViewById(R.id.text_edit);
+        String x = (String) e.getText();
+        e.setText(x+"4");
+    }
+    public void onClickFive(View view){
+    TextView e = (TextView) findViewById(R.id.text_edit);
+        String x = (String) e.getText();
+        e.setText(x+"5");
+    }
+    public void onClickSix(View view){
+    TextView e = (TextView) findViewById(R.id.text_edit);
+        String x = (String) e.getText();
+        e.setText(x+"6");
+    }
+    public void onClickSeven(View view){
+    TextView e = (TextView) findViewById(R.id.text_edit);
+        String x = (String) e.getText();
+        e.setText(x+"7");
+    }
+    public void onClickEight(View view){
+    TextView e = (TextView) findViewById(R.id.text_edit);
+        String x = (String) e.getText();
+        e.setText(x+"8");
+    }
+    public void onClickNine(View view){
+    TextView e = (TextView) findViewById(R.id.text_edit);
+        String x = (String) e.getText();
+        e.setText(x+"9");
+    }
+    public void onClickZero(View view){
+    TextView e = (TextView) findViewById(R.id.text_edit);
+        String x = (String) e.getText();
+        e.setText(x+"0");
+    }
+    public void onClickDot(View view){
+        TextView e = (TextView) findViewById(R.id.text_edit);
+        String a = (String) e.getText();
+        e.setText(a+".");
 
     }
+
+    public void onClickEqual(View view){
+        TextView e = (TextView) findViewById(R.id.text_edit);
+        TextView f = (TextView) findViewById(R.id.show_prev);
+        TextView g = (TextView) findViewById(R.id.show_operator);
+
+        String x = (String) g.getText();
+
+        if(x.equals("+")){
+
+            float a = Float.parseFloat((String) f.getText());
+            float b = Float.parseFloat((String) e.getText());
+            float c =a+b;
+            e.setText(Float.toString(c));
+
+        }
+
+        if(x.equals("-")){
+
+            float a = Float.parseFloat((String) f.getText());
+            float b = Float.parseFloat((String) e.getText());
+            float c = a-b;
+            e.setText(Float.toString(c));
+
+        }
+
+        if(x.equals("*")){
+
+            float a = Float.parseFloat((String) f.getText());
+            float b = Float.parseFloat((String) e.getText());
+            float c = a*b;
+            e.setText(Float.toString(c));
+
+        }
+
+        if(x.equals("/")){
+
+            float a = Float.parseFloat((String) f.getText());
+            float b = Float.parseFloat((String) e.getText());
+            float c = a/b;
+            e.setText(Float.toString(c));
+
+        }
+
+
+
+    }
+
+    public void onClickAdd(View view){
+        TextView e = (TextView) findViewById(R.id.text_edit);
+        TextView f = (TextView) findViewById(R.id.show_prev);
+        TextView g = (TextView) findViewById(R.id.show_operator);
+
+        g.setText("+");
+        f.setText(e.getText());
+        e.setText("");
+
+        //float a = Float.parseFloat((String) e.getText());
+        //float b = Float.parseFloat((String) e.getText());
+        //float c = a+b;
+
+        //while(onClickEqual(view))
+        //e.setText(Float.toString(c));
+
+    }
+
+    public void onClickSubtract(View view) {
+        TextView e = (TextView) findViewById(R.id.text_edit);
+        TextView f = (TextView) findViewById(R.id.show_prev);
+        TextView g = (TextView) findViewById(R.id.show_operator);
+
+        g.setText("-");
+        f.setText(e.getText());
+        e.setText("");
+    }
+
+    public void onClickMultiply(View view) {
+        TextView e = (TextView) findViewById(R.id.text_edit);
+        TextView f = (TextView) findViewById(R.id.show_prev);
+        TextView g = (TextView) findViewById(R.id.show_operator);
+
+        g.setText("*");
+        f.setText(e.getText());
+        e.setText("");
+    }
+
+    public void onClickDivide(View view) {
+        TextView e = (TextView) findViewById(R.id.text_edit);
+        TextView f = (TextView) findViewById(R.id.show_prev);
+        TextView g = (TextView) findViewById(R.id.show_operator);
+
+        g.setText("/");
+        f.setText(e.getText());
+        e.setText("");
+    }
+
+    public void onClickBackspace(View view){
+        TextView e = (TextView) findViewById(R.id.text_edit);
+        String s = "";
+        String t = (String) e.getText();
+
+        for(int i =0;i<t.length()-1;i++){
+            s = s+t.charAt(i);
+            e.setText(s);
+        }
+
+
+    }
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
